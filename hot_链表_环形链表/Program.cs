7ee4edb415648293556428c74aad250307e19732
PixiY26G <3422ssggbbz@proton.me>
2026-01-08 16:40:01 +0800

    2026/1/8 测试提交

---
+++ hot_链表_环形链表/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace hot_链表_环形链表
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public class Solution
+        {
+            public bool HasCycle(ListNode head)
+            {
+                if (head == null)
+                {
+                    return false;
+                }
+                bool res = false;
+                ListNode fast = head;
+                ListNode slow = head;
+                while (fast.next != null && fast.next.next != null && fast != null)
+                {
+
+                    if (fast == slow)
+                    {
+                        return true;
+                    }
+                    fast = fast.next.next;
+                    slow = slow.next;
+                }
+                return false;
+            }
+        }
+    }
+}
